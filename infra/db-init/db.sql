0cd7adc5b0935fef9d0c2ba93eb5cea6003b7bc4
Diego Roman Rodriguez <d.rodriguez@sig.eu>
2022-12-26 13:46:45 +0100

    Update db model

--- infra/db-init/db.sql
+++ infra/db-init/db.sql
@@ -1,29 +1,40 @@
 CREATE TABLE partner
 (
-    id  SERIAL PRIMARY KEY NOT NULL,
+    id  SERIAL NOT NULL,
     name   VARCHAR(50) NOT NULL,
     email      VARCHAR(255) UNIQUE NOT NULL,
     created_on  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    active  BOOLEAN NOT NULL
+    active  BOOLEAN NOT NULL,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE customer
 (
-    id  SERIAL PRIMARY KEY NOT NULL,
+    id  SERIAL NOT NULL,
+    partner_id integer NOT NULL,
     name     VARCHAR(100) NOT NULL ,
     created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    active BOOLEAN NOT NULL
+    active BOOLEAN NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_partner
+        FOREIGN KEY(partner_id)
+            REFERENCES partner(id)
 );
 
 CREATE TABLE api_key
 (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id SERIAL NOT NULL,
+    customer_id integer NOT NULL,
     created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     key VARCHAR(32) NOT NULL,
-    active BOOLEAN NOT NULL
+    active BOOLEAN NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT fk_customer
+        FOREIGN KEY(customer_id)
+            REFERENCES customer(id)
 );
 
-INSERT INTO public.partner (id, name, email, created_on, active)
-VALUES (1, 'Partner1', 'prtnr1@prtnr.com', '2022-02-18 19:46:57.000000', true);
-INSERT INTO public.partner (id, name, email, created_on, active)
-VALUES (2, 'Partner2', 'prtnr2@prtnr.com', '2022-02-20 19:46:57.000000', false);
+INSERT INTO public.partner (name, email, created_on, active)
+VALUES ('Partner1', 'prtnr1@prtnr.com', '2022-02-18 19:46:57.000000', true);
+INSERT INTO public.partner (name, email, created_on, active)
+VALUES ('Partner2', 'prtnr2@prtnr.com', '2022-02-20 19:46:57.000000', false);
